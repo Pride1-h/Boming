1e2b3ca642fca75f464ec99e3a18ef23f3344336
Pride1-h <57586602+Pride1-h@users.noreply.github.com>
2020-03-07 10:36:33 -0500

    Update BOMMING.sh

--- BOMMING.sh
+++ BOMMING.sh
@@ -2,7 +2,7 @@
 clear
 echo -e "\e[4;31m pride1-h Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m TBomb \e[0m"
+echo -e "\e[1;32m Bomming \e[0m"
 echo "Press Enter To Continue"
 read a1
 if [[ -s update.pride1-h ]];then
